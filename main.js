d41fe5951fbd642ee0f329866da69ac1573af7fb
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-01-09 22:42:21 -0300

    Adicao das classes do projeto

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-console.log("Arquivo JavaScript criado com sucesso!");
+class Book {
+    constructor (title, description, author){
+        this.id = this.gerarId()
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+}
+
+class books {
+    constructor(){
+        this.books = [];
+    }
+
+    addBook(bookInfo) {
+        const book = new Book(bookInfo.title, bookInfo.description, bookInfo.author);
+        this.books.push(book);
+        return book;
+    }
+
+    getBooks(){
+        return this.books;
+    }
+
+    removeBook(id){
+        this.books = this.books.splice(id,1)
+    }
+
+    getBook(id){
+        return this.books.find(book => book.id === id);
+    }
+    updateBookById(id, info) {
+        const book = this.getBookById(id);
+        if (book) {
+          book.title = info.title || book.title;
+          book.description = info.description || book.description;
+          book.author = info.author || book.author;
+          return book;
+        }
+        return null;
+    }
+}
